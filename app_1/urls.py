09c94445b7a6e934a085223bd530accc209df991
hyonbokan <mkkanhb@gmail.com>
2024-03-12 14:57:16 +0900

    improvement comments

--- app_1/urls.py
+++ app_1/urls.py
@@ -4,7 +4,6 @@ from app_1.views import *
 from . import views
 
 urlpatterns = [
-    path('query_detail/<slug:slug>',views.QUERY_DETAIL,name='query_detail'),
     path('bgp_llama',views.BGP_LLaMA,name='bgp_llama'),
     path('dataset', views.DATASET, name='dataset'),
     # path('', )

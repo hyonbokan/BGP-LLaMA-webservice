247995b89dde81f684bb5c500a749271fd7900cb
hyonbokan <mkkanhb@gmail.com>
2024-04-26 15:44:04 +0900

    front end bug fixed

---
+++ react_frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-gray-900 text-white text-center p-4">
+      <h1 className="text-3xl font-bold">BGP-LLaMA</h1>
+    </header>
+  );
+};
+
+export default Header;

428bc1fee8e1b29734b27133cde0fdfe5499b9b0
hyonbokan <mkkanhb@gmail.com>
2024-04-29 16:36:20 +0900

    dataset grid added

--- react_frontend/src/features/DatasetPage.js
+++ react_frontend/src/features/DatasetPage.js
@@ -1,28 +1,129 @@
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
-
-
+import React from 'react';
+import { Grid, Card, CardContent, CardMedia, Typography, IconButton } from '@mui/material';
 
 const DatasetPage = () => {
-    // URLs for downloading the files
-    const fileUrls = {
-        bgpKnowledge: '',
-        pyBGPStreamBase: '',
-        pyBGPStreamRealTime: '',
-    }
+
+    const sections = [
+        {
+            title: 'Self-Instruct Generated Dataset',
+            datasets: [
+                {
+                    title:'BGP Knowledge',
+                    imageUrl: '',
+                    fileCount: 1111,
+                    size: '10 MB',
+                    fileType: 'json'
+
+                },
+                {
+                    title:'PyBGPStream Base',
+                    imageUrl: '',
+                    fileCount: 1111,
+                    size: '10 MB',
+                    fileType: 'json'
+
+                },
+                {
+                    title:'PyBGPStream Real-time Stream',
+                    imageUrl: '',
+                    fileCount: 1111,
+                    size: '10 MB',
+                    fileType: 'json'
+
+                },
+            ],
+        },
+        {
+            title: 'Manual Seed Tasks',
+            datasets: [
+                {
+                    title:'BGP Knowledge I',
+                    imageUrl: '',
+                    fileCount: 1111,
+                    size: '10 MB',
+                    fileType: 'json'
+
+                },
+                {
+                    title:'BGP Knowledge II',
+                    imageUrl: '',
+                    fileCount: 1111,
+                    size: '10 MB',
+                    fileType: 'json'
+
+                },
+                {
+                    title:'PyBGPStream Base',
+                    imageUrl: '',
+                    fileCount: 1111,
+                    size: '10 MB',
+                    fileType: 'json'
+
+                },
+                {
+                    title:'PyBGPStream Real Cases',
+                    imageUrl: '',
+                    fileCount: 1111,
+                    size: '10 MB',
+                    fileType: 'json'
+
+                },
+                {
+                    title:'PyBGPStream Real-time Stream',
+                    imageUrl: '',
+                    fileCount: 1111,
+                    size: '10 MB',
+                    fileType: 'json'
+
+                },             
+            ]
+        }
+    ];
+
     return (
         <div>
             <Header />
             <Navbar />
-            <h1>Training Data</h1>
-            <ul>
-                <li><a href={fileUrls.bgpKnowledge} download>BGP Knowledge</a></li>
-                <li><a href={fileUrls.pyBGPStreamBase} download>PyBGPStream Base</a></li>
-                <li><a href={fileUrls.pyBGPStreamRealTime} download>PyBGPStream Real-time Stream</a></li>
-            </ul>
-            <h1>Manual Seed Tasks</h1>
+            <div>
+                {sections.map((section, sectionIndex) => (
+                    <div
+                        key={sectionIndex}
+                        // sx={{ mb: 4}}
+                        style={{ marginTop:'50px', marginBottom:'50px'}}
+                    >
+                        <Typography variant='h6' gutterBottom>
+                            {section.title}
+                        </Typography>
+                        <Grid container spacing={2}>
+                            {section.datasets.map((dataset, index) => (
+                                <Grid item xs={12} sm={6} md={3} key={index}>
+                                    <Card>
+                                        {/* <CardMedia
+                                        component='img'
+                                        height='140'
+                                        image={dataset.imageUrl}
+                                        alt={dataset.title}/> */}
+                                        <CardContent>
+                                            <Typography variant='subtitle1'>{dataset.title}</Typography>
+                                            <Typography variant='body2'>Instruction count: {dataset.fileCount}</Typography>
+                                            <Typography variant='body2'>File size: {dataset.size}</Typography>
+                                            <Typography variant='body2'>Type: {dataset.fileType}</Typography>
+                                        </CardContent>
+                                        <IconButton aria-label='settings' size='large'>
+
+                                        </IconButton>
+
+                                    </Card> 
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </div>
+                ))}
+            </div>
         </div>
     );
-};
+}
 
 export default DatasetPage;

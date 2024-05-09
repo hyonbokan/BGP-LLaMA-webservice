fba1861daa36f6b4482d1837994d3fcd12fed34f
hyonbokan <mkkanhb@gmail.com>
2024-05-09 17:21:06 +0900

    func name changed and  NotFoundPage added

---
+++ react_frontend/src/features/NotFoundPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <Typography variant='h3' color='error'>
+        404 - Page Not Found
+      </Typography>
+    </div>
+  );
+};
+
+export default NotFoundPage;

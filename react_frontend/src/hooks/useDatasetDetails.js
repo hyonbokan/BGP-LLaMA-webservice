87e340c808f1ec2df0190ccb443558d3070f26f4
hyonbokan <mkkanhb@gmail.com>
2024-09-08 23:04:27 +0900

    updated detailpage UI

---
+++ react_frontend/src/hooks/useDatasetDetails.js
@@ -0,0 +1,40 @@
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const useDatasetDetails = () => {
+    const { sectionId, datasetId } = useParams();
+
+    // Fetch dataset details from Redux store
+    const dataset = useSelector(state => {
+        const section = state.datasets.allDatasets.find(sec => sec.id === sectionId);
+        return section ? section.datasets.find(ds => ds.id === datasetId) : null;
+    });
+
+    // File download handler
+    const handleDownload = (fileUrl, fileName, fileExtension = '') => {
+        const relativeFileUrl = fileUrl.replace(/^\/+/, '');
+        const url = `http://127.0.0.1:8000/api/download/?file=${encodeURIComponent(relativeFileUrl)}`;
+
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`,
+            }
+        })
+        .then(response => response.blob())
+        .then(blob => {
+            const downloadUrl = window.URL.createObjectURL(new Blob([blob]));
+            const link = document.createElement('a');
+            link.href = downloadUrl;
+            link.setAttribute('download', `${fileName}${fileExtension ? '.' + fileExtension : ''}`);
+            document.body.appendChild(link);
+            link.click();
+            link.parentNode.removeChild(link);
+        })
+        .catch(error => console.error('Error downloading the file: ', error));
+    };
+
+    return { dataset, sectionId, handleDownload };
+};
+
+export default useDatasetDetails;
